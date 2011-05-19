9946a27c55d9a0e6ea3cd8d06132ea9357a6aa15
Ioannis Canellos <iocanel@apache.org>
2011-05-19 18:20:02 +0000

    [KARAF-628] Added cellar event,synchronizer,listener and handler for replicating bundle states
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1125040 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundle/src/main/java/org/apache/karaf/cellar/bundle/BundleEventHandler.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.karaf.cellar.bundle;
+
+
+import org.apache.karaf.cellar.core.Configurations;
+import org.apache.karaf.cellar.core.Group;
+import org.apache.karaf.cellar.core.Node;
+import org.apache.karaf.cellar.core.control.BasicSwitch;
+import org.apache.karaf.cellar.core.control.Switch;
+import org.apache.karaf.cellar.core.event.EventHandler;
+import org.apache.karaf.cellar.core.event.EventType;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * @author iocanel
+ */
+public class BundleEventHandler extends BundleSupport implements EventHandler<RemoteBundleEvent> {
+
+	private static final Logger logger = LoggerFactory.getLogger(BundleEventHandler.class);
+	public static final String SWITCH_ID = "com.upstreamsystems.curry.cluster.bundle.handler";
+
+	private final Switch eventSwitch = new BasicSwitch(SWITCH_ID);
+    private Node node;
+
+	/**
+	 * Handles remote bundle events.
+	 * @param event
+	 */
+	public void handle(RemoteBundleEvent event) {
+
+        if (event == null || event.getSourceGroup() == null || node == null || node.equals(event.getSourceNode()))
+            return;
+
+        Group group = event.getSourceGroup();
+        String groupName = group.getName();
+        String bundleLocation = event.getLocation();
+
+        Map<String, BundleState> bundleTable = clusterManager.getMap(Constants.BUNDLE_MAP + Configurations.SEPARATOR + groupName);
+
+		try {
+             //Check if the pid is marked as local.
+            if (isAllowed(event.getSourceGroup(), Constants.CATEGORY, bundleLocation, EventType.INBOUND)) {
+				BundleState state = new BundleState();
+				state.setLocation(event.getLocation());
+				state.setStatus(event.getType());
+
+				if(event.getType() == BundleEvent.INSTALLED) {
+					installBundleFromLocation(event.getLocation());
+					bundleTable.put(event.getId(),state);
+				} else if(event.getType() == BundleEvent.UNINSTALLED) {
+					uninstallBundle(event.getSymbolicName(), event.getVersion());
+					bundleTable.remove(event.getId());
+				} else if(event.getType() == BundleEvent.STARTED) {
+					 startBundle(event.getSymbolicName(),event.getVersion());
+					 bundleTable.put(event.getId(),state);
+				} else if(event.getType() == BundleEvent.STOPPED) {
+					 stopBundle(event.getSymbolicName(), event.getVersion());
+					 state.setStatus(BundleEvent.INSTALLED);
+					 bundleTable.put(event.getId(),state);
+				} else if(event.getType() == BundleEvent.UPDATED) {
+					updateBundle(event.getSymbolicName(), event.getVersion());
+				}
+			} else logger.debug("Bundle with symbolicName {} is marked as BLOCKED INBOUND",event.getSymbolicName());
+		} catch (BundleException e) {
+			logger.info("Failed to install bundle.");
+		}
+	}
+
+
+    /**
+     * Initialization Method.
+     */
+    public void init() {
+        if (clusterManager != null) {
+            node = clusterManager.getNode();
+        }
+    }
+
+    /**
+     * Destruction Method.
+     */
+    public void destroy() {
+
+    }
+
+    public Switch getSwitch() {
+        return eventSwitch;
+    }
+
+    public Class<RemoteBundleEvent> getType() {
+        return RemoteBundleEvent.class;
+    }
+}
