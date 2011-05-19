dc23a64501c01d7849ae6feeb5d3a309476100b8
iocanel <iocanel@13f79535-47bb-0310-9956-ffa450edef68>
2011-05-19 18:20:02 +0000

    [KARAF-628] Added cellar event,synchronizer,listener and handler for replicating bundle states
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1125040 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundle/src/main/java/org/apache/karaf/cellar/bundle/BundleSupport.java
@@ -0,0 +1,158 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.karaf.cellar.bundle;
+
+import org.apache.karaf.cellar.core.CellarSupport;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * @author iocanel
+ */
+public class BundleSupport extends CellarSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(CellarSupport.class);
+
+	protected BundleContext bundleContext;
+
+	/**
+	 * Reads a {@code Dictionary} object and creates a property object out of it.
+	 *
+	 * @param dictionary
+	 * @return
+	 */
+	public Properties dictionaryToProperties(Dictionary dictionary) {
+		Properties properties = new Properties();
+		if (dictionary != null && dictionary.keys() != null) {
+
+			Enumeration keys = dictionary.keys();
+			while (keys.hasMoreElements()) {
+				String key = (String) keys.nextElement();
+				if (key != null && dictionary.get(key) != null) {
+					properties.put(key, dictionary.get(key));
+				}
+			}
+		}
+		return properties;
+	}
+
+
+	/**
+	 * Installs a bundle using its location.
+	 */
+	public void installBundleFromLocation(String location) throws BundleException {
+	   getBundleContext().installBundle(location);
+	}
+
+	/**
+	 * Uninstalls a bundle using its Symbolic name and version.
+	 * @param symbolicName
+	 * @param version
+	 * @throws BundleException
+	 */
+	public void uninstallBundle(String symbolicName, String version) throws BundleException {
+	   Bundle[] bundles = getBundleContext().getBundles();
+	   if(bundles != null) {
+		   for(Bundle bundle:bundles) {
+			   if(bundle.getSymbolicName().equals(symbolicName) && bundle.getVersion().toString().equals(version)) {
+				   bundle.uninstall();
+			   }
+		   }
+	   }
+	}
+
+    /**
+	 * Starts a bundle using its Symbolic name and version.
+	 * @param symbolicName
+	 * @param version
+	 * @throws BundleException
+	 */
+	public void startBundle(String symbolicName, String version) throws BundleException {
+	   Bundle[] bundles = getBundleContext().getBundles();
+	   if(bundles != null) {
+		   for(Bundle bundle:bundles) {
+			   if(bundle.getSymbolicName().equals(symbolicName) && bundle.getVersion().toString().equals(version)) {
+				   bundle.start();
+			   }
+		   }
+	   }
+	}
+
+	/**
+	 * Stops a bundle using its Symbolic name and version.
+	 * @param symbolicName
+	 * @param version
+	 * @throws BundleException
+	 */
+	public void stopBundle(String symbolicName, String version) throws BundleException {
+	   Bundle[] bundles = getBundleContext().getBundles();
+	   if(bundles != null) {
+		   for(Bundle bundle:bundles) {
+			   if(bundle.getSymbolicName().equals(symbolicName) && bundle.getVersion().toString().equals(version)) {
+				   bundle.stop();
+			   }
+		   }
+	   }
+	}
+
+	/**
+	 * Updates a bundle using its Symbolic name and version.
+	 * @param symbolicName
+	 * @param version
+	 * @throws BundleException
+	 */
+	public void updateBundle(String symbolicName, String version) throws BundleException {
+	   Bundle[] bundles = getBundleContext().getBundles();
+	   if(bundles != null) {
+		   for(Bundle bundle:bundles) {
+			   if(bundle.getSymbolicName().equals(symbolicName) && bundle.getVersion().toString().equals(version)) {
+				   bundle.update();
+			   }
+		   }
+	   }
+	}
+
+	/**
+	 * Returns the {@link BundleContext}.
+	 * @return
+	 */
+	public BundleContext getBundleContext() {
+		return this.bundleContext;
+	}
+
+	/**
+	 * Sets the {@link BundleContext}.
+	 * @param bundleContext
+	 */
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+	public ConfigurationAdmin getConfigurationAdmin() {
+		return configurationAdmin;
+	}
+
+	public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+		this.configurationAdmin = configurationAdmin;
+	}
+}
