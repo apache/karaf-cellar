a83f197df4cbbb51fc9186da265e0f4ea6a559a9
iocanel <iocanel@13f79535-47bb-0310-9956-ffa450edef68>
2011-08-27 13:54:04 +0000

    [KARAF-837] Added samples. Added dosgi-greeter sample. Added hazelcast-app sample.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1162356 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/dosgi-greeter/api/src/main/java/org/apache/karaf/cellar/samples/dosgi/greeter/api/GreetResponse.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cellar.samples.dosgi.greeter.api;
+
+import java.io.Serializable;
+
+/**
+ * @author: iocanel
+ */
+public class GreetResponse implements Serializable {
+
+    private Greet greet;
+    private String response;
+
+    public GreetResponse(Greet greet, String response) {
+        this.greet = greet;
+        this.response = response;
+    }
+
+    public Greet getGreet() {
+        return greet;
+    }
+
+    public void setGreet(Greet greet) {
+        this.greet = greet;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
