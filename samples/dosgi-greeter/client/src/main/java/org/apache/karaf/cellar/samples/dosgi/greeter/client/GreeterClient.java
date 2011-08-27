cb254252f55b29209d56ff9754b6e5317978b066
iocanel <iocanel@13f79535-47bb-0310-9956-ffa450edef68>
2011-08-27 14:55:41 +0000

    Merged from trunk: [KARAF-837] Added samples. Added dosgi-greeter sample. Added hazelcast-app sample. [KARAF-837] Added missing files. revisions: 1162356 & 1162357
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1162366 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/dosgi-greeter/client/src/main/java/org/apache/karaf/cellar/samples/dosgi/greeter/client/GreeterClient.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cellar.samples.dosgi.greeter.client;
+
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greet;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.GreetResponse;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greeter;
+
+/**
+ * @author: iocanel
+ */
+public class GreeterClient {
+
+    private Greeter greeter;
+    private String greetMessage;
+    private int count;
+
+    public GreeterClient(Greeter greeter, String greetMessage, int count) {
+        this.greeter = greeter;
+        this.greetMessage = greetMessage;
+        this.count = count;
+    }
+
+    public void start() {
+        Greet greet  = new Greet(greetMessage);
+        for (int i = 0; i < count; i++) {
+            GreetResponse greetResponse = greeter.greet(greet);
+            if(greetResponse != null) {
+                System.out.println(greetResponse.getResponse());
+            } else System.out.println("Time out!");
+        }
+    }
+}
---
+++ samples/hazelcast-app/src/main/java/org/apache/karaf/cellar/samples/hazelcast/Activator.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cellar.samples.hazelcast;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ITopic;
+import com.hazelcast.core.IdGenerator;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author: iocanel
+ */
+public class Activator implements BundleActivator {
+
+    private static final transient Logger LOGGER = LoggerFactory.getLogger(Activator.class);
+
+    private ITopic topic;
+    private MessageListener messageListener = new MessageListener();
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        ServiceReference reference = context.getServiceReference("com.hazelcast.core.HazelcastInstance");
+        HazelcastInstance instance = (HazelcastInstance) context.getService(reference);
+        context.ungetService(reference);
+        try {
+            IdGenerator idGenerator = instance.getIdGenerator("cellar-smaple-generator");
+            Long id = idGenerator.newId();
+            topic = instance.getTopic("cellar-sample-topic");
+            topic.addMessageListener(messageListener);
+            topic.publish(new Message("id="+id));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        Thread.currentThread().setContextClassLoader(classLoader);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        topic.removeMessageListener(messageListener);
+    }
+}
