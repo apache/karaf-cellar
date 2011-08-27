4f40c436aa167130a21373f2f27efe26f2071bf2
Ioannis Canellos <iocanel@apache.org>
2011-08-27 13:54:04 +0000

    [KARAF-837] Added samples. Added dosgi-greeter sample. Added hazelcast-app sample.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1162356 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/dosgi-greeter/client/src/main/java/org/apache/karaf/cellar/samples/dosgi/greeter/client/GreetCommand.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cellar.samples.dosgi.greeter.client;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greeter;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+/**
+ * @author: iocanel
+ */
+@Command(scope = "dosgi-greeter", name = "greet", description = "Starts the greet client")
+public class GreetCommand extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "greetMessage", description = "The message that will be sent as greet.", required = true, multiValued = false)
+    String greetMessage;
+
+    @Argument(index = 1, name = "iterations", description = "The number of greet iterations to perform", required = false, multiValued = false)
+    Integer iterations = 10;
+
+     private Greeter greeter;
+
+
+    protected Object doExecute() throws Exception {
+        GreeterClient greeterClient = new GreeterClient(greeter, greetMessage,iterations);
+        greeterClient.start();
+        return null;
+    }
+
+    public Greeter getGreeter() {
+        return greeter;
+    }
+
+    public void setGreeter(Greeter greeter) {
+        this.greeter = greeter;
+    }
+}
---
+++ samples/dosgi-greeter/service/src/main/java/org/apache/karaf/cellar/samples/dosgi/greeter/service/GreeterImpl.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cellar.samples.dosgi.greeter.service;
+
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greet;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.GreetResponse;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greeter;
+
+/**
+ * @author: iocanel
+ */
+public class GreeterImpl implements Greeter {
+
+    private int counter=0;
+    private String id;
+
+    public GreeterImpl(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public GreetResponse greet(Greet greet) {
+        String message = greet.getMessage();
+        String response = message+"." +String.format("Halo from node %s count %s.",id,counter++);
+        GreetResponse greetResponse = new GreetResponse(greet,response);
+        return greetResponse;
+    }
+}
