eee96053265d8aeb851da830046dc5ac75abd611
jbonofre <jbonofre@13f79535-47bb-0310-9956-ffa450edef68>
2011-09-14 14:28:45 +0000

    [KARAF-857] Add the OBR command support.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1170640 13f79535-47bb-0310-9956-ffa450edef68

---
+++ obr/src/main/java/org/apache/karaf/cellar/obr/shell/ObrCommandSupport.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.cellar.obr.shell;
+
+import org.apache.felix.bundlerepository.RepositoryAdmin;
+import org.apache.karaf.cellar.core.shell.CellarCommandSupport;
+
+public abstract class ObrCommandSupport extends CellarCommandSupport {
+
+    protected RepositoryAdmin obrService;
+
+    public RepositoryAdmin getObrService() {
+        return this.obrService;
+    }
+
+    public void setObrService(RepositoryAdmin obrService) {
+        this.obrService = obrService;
+    }
+
+    public abstract Object doExecute() throws Exception;
+
+
+}
