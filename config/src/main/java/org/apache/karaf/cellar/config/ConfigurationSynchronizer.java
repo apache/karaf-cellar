ab6a38f3720e42e8d6fda7c77281a11bfc61a23d
iocanel <iocanel@13f79535-47bb-0310-9956-ffa450edef68>
2011-12-02 22:51:13 +0000

    Merged from trunk [KARAF-1034] Fixed destroy methods so that cellar can cleanly uninstall. Added some lifecycle and npe checks. [from revision 1209755]
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1209761 13f79535-47bb-0310-9956-ffa450edef68

--- config/src/main/java/org/apache/karaf/cellar/config/ConfigurationSynchronizer.java
+++ config/src/main/java/org/apache/karaf/cellar/config/ConfigurationSynchronizer.java
@@ -175,9 +175,11 @@ public class ConfigurationSynchronizer extends ConfigurationSupport implements S
         try {
             Configuration configuration = configurationAdmin.getConfiguration(Configurations.GROUP);
             Dictionary<String, String> properties = configuration.getProperties();
-            String propertyKey = groupName + Configurations.SEPARATOR + Constants.CATEGORY + Configurations.SEPARATOR + Configurations.SYNC;
-            String propertyValue = properties.get(propertyKey);
-            result = Boolean.parseBoolean(propertyValue);
+            if (properties != null) {
+                String propertyKey = groupName + Configurations.SEPARATOR + Constants.CATEGORY + Configurations.SEPARATOR + Configurations.SYNC;
+                String propertyValue = properties.get(propertyKey);
+                result = Boolean.parseBoolean(propertyValue);
+            }
         } catch (IOException e) {
             LOGGER.error("CELLAR CONFIG: failed to check if sync is enabled", e);
         }
