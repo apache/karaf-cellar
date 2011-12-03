d28e0f01e7886c1a057b7705567688eb6fe8e2f0
Ioannis Canellos <iocanel@apache.org>
2011-12-03 19:15:18 +0000

    Merged from trunk [KARAF-1082] Added a unit test for the Hazlecast configuration manager that tests isUpdated method. Also removed tests that now belong to CellarUtilsTest. [from revision 1209976]
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1209984 13f79535-47bb-0310-9956-ffa450edef68

--- hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastConfigurationManagerTest.java
+++ hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastConfigurationManagerTest.java
@@ -15,30 +15,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class HazelcastConfigurationManagerTest {
 
-    @Test
-    public void testCollectionEquals() {
-        HazelcastConfigurationManager cm = new HazelcastConfigurationManager();
-        //Lists
-        Assert.assertTrue(cm.collectionEquals(null,null));
-        Assert.assertTrue(cm.collectionEquals(new LinkedList(),new LinkedList()));
-        Assert.assertTrue(cm.collectionEquals(new LinkedList(),new ArrayList()));
 
-        Assert.assertTrue(cm.collectionEquals(Arrays.asList(""),Arrays.asList("")));
-        Assert.assertTrue(cm.collectionEquals(Arrays.asList("1"),Arrays.asList("1")));
-        Assert.assertFalse(cm.collectionEquals(Arrays.asList("1"), Arrays.asList("2")));
-        Assert.assertTrue(cm.collectionEquals(Arrays.asList("1","2"),Arrays.asList("2","1")));
-        Assert.assertFalse(cm.collectionEquals(Arrays.asList("1", "2"), Arrays.asList("2", "1", "3")));
-
-        //Sets
-        Assert.assertTrue(cm.collectionEquals(new LinkedHashSet(),new LinkedHashSet()));
-        Set<String> s1 = new LinkedHashSet<String>();
-        Set<String> s2 = new LinkedHashSet<String>();
-
-        s1.add("");
-        s2.add("");
-        Assert.assertTrue(cm.collectionEquals(s1,s2));
-        System.out.println(String.format("%s %s",s1,s2));
-    }
 
     @Test
     public void testIsUpdatedWithNoProperties()  {
