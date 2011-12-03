d468915c74ed39763dea0f085f2af2a9a62f617a
iocanel <iocanel@13f79535-47bb-0310-9956-ffa450edef68>
2011-12-03 18:23:54 +0000

    [KARAF-1081] Added a merge policy for cellar. The merge policy will merge string (comma separated & specially tagged), sets, list and multinode entries.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1209964 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/karaf/cellar/core/utils/CellarUtilsTest.java
@@ -0,0 +1,77 @@
+package org.apache.karaf.cellar.core.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.regex.Pattern;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CellarUtilsTest {
+
+    @Test
+    public void testCollectionEquals() {
+
+        //Lists
+        Assert.assertTrue(CellarUtils.collectionEquals(null, null));
+        Assert.assertTrue(CellarUtils.collectionEquals(new LinkedList(),new LinkedList()));
+        Assert.assertTrue(CellarUtils.collectionEquals(new LinkedList(),new ArrayList()));
+
+        Assert.assertTrue(CellarUtils.collectionEquals(Arrays.asList(""),Arrays.asList("")));
+        Assert.assertTrue(CellarUtils.collectionEquals(Arrays.asList("1"),Arrays.asList("1")));
+        Assert.assertFalse(CellarUtils.collectionEquals(Arrays.asList("1"), Arrays.asList("2")));
+        Assert.assertTrue(CellarUtils.collectionEquals(Arrays.asList("1","2"),Arrays.asList("2","1")));
+        Assert.assertFalse(CellarUtils.collectionEquals(Arrays.asList("1", "2"), Arrays.asList("2", "1", "3")));
+
+        //Sets
+        Assert.assertTrue(CellarUtils.collectionEquals(new LinkedHashSet(),new LinkedHashSet()));
+        Set<String> s1 = new LinkedHashSet<String>();
+        Set<String> s2 = new LinkedHashSet<String>();
+
+        s1.add("");
+        s2.add("");
+        Assert.assertTrue(CellarUtils.collectionEquals(s1,s2));
+        System.out.println(String.format("%s %s",s1,s2));
+    }
+
+    @Test
+    public void testMergableRegex() {
+        Pattern p = Pattern.compile(CellarUtils.MERGABLE_REGEX);
+        Assert.assertEquals(true,p.matcher(String.format(CellarUtils.MERGABLE,"")).matches());
+        Assert.assertEquals(true,p.matcher(String.format(CellarUtils.MERGABLE,"item1")).matches());
+        Assert.assertEquals(true,p.matcher(String.format(CellarUtils.MERGABLE,"item1,item2")).matches());
+        Assert.assertEquals(true,p.matcher(String.format(CellarUtils.MERGABLE,"item1, item2")).matches());
+    }
+
+
+    @Test
+    public void testSetFromString() {
+        Set<String> itemSet = new LinkedHashSet<String>();
+        String items = "item1,item2";
+        itemSet.add("item1");
+        itemSet.add("item2");
+
+        Set<String> resultSet = CellarUtils.createSetFromString(items);
+        Assert.assertTrue(CellarUtils.collectionEquals(itemSet,resultSet));
+
+        resultSet = CellarUtils.createSetFromString(String.format(CellarUtils.MERGABLE,items));
+        Assert.assertTrue(CellarUtils.collectionEquals(itemSet,resultSet));
+    }
+
+    @Test
+    public void testStringFromSet() {
+        Set<String> itemSet = new LinkedHashSet<String>();
+        String items = "item1,item2";
+        itemSet.add("item1");
+        itemSet.add("item2");
+
+        String result = CellarUtils.createStringFromSet(itemSet,false);
+        Assert.assertEquals(items,result);
+
+        result = CellarUtils.createStringFromSet(itemSet,true);
+        Assert.assertEquals(String.format(CellarUtils.MERGABLE,items),result);
+    }
+
+}
