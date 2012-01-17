214e936af438ed5254a7babe571f3bb4a5258b18
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-01-17 08:31:47 +0000

    Fix HazelcastServiceFactoryTest
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1232333 13f79535-47bb-0310-9956-ffa450edef68

--- hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastConfigurationManagerTest.java
+++ hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastConfigurationManagerTest.java
@@ -13,17 +13,12 @@
  */
 package org.apache.karaf.cellar.hazelcast.factory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Properties;
-import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Properties;
 
 @RunWith(JUnit4.class)
 public class HazelcastConfigurationManagerTest {
@@ -41,4 +36,5 @@ public class HazelcastConfigurationManagerTest {
         p.put(HazelcastConfigurationManager.TCPIP_MEMBERS,"");
         Assert.assertFalse(cm.isUpdated(p));
     }
+
 }
