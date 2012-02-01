20e4aff6d9ff522c72636f226d94b0523e33688f
jbonofre <jbonofre@13f79535-47bb-0310-9956-ffa450edef68>
2012-02-01 07:22:19 +0000

    [KARAF-861] Add cluster group support for bundle
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/trunk@1238955 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundle/src/main/java/org/apache/karaf/cellar/bundle/shell/ListBundleCommand.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.cellar.bundle.shell;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.cellar.bundle.BundleState;
+import org.apache.karaf.cellar.bundle.Constants;
+import org.apache.karaf.cellar.core.Configurations;
+import org.apache.karaf.cellar.core.Group;
+import org.apache.karaf.cellar.core.shell.CellarCommandSupport;
+import org.osgi.framework.BundleEvent;
+
+import java.util.Map;
+
+@Command(scope = "cluster", name = "bundle-list", description = "List the bundles assigned to a cluster group.")
+public class ListBundleCommand extends CellarCommandSupport {
+
+    protected static final String OUTPUT_FORMAT = "%-50s %-20s %-15s %-20s";
+
+    @Argument(index = 0, name = "group", description = "The cluster group name.", required = true, multiValued = false)
+    String groupName;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        Group group = groupManager.findGroupByName(groupName);
+
+        if (group == null) {
+            System.err.println("Cluster group " + groupName + " doesn't exist");
+            return null;
+        }
+
+        ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        try {
+            Map<String, BundleState> bundles = clusterManager.getMap(Constants.BUNDLE_MAP + Configurations.SEPARATOR + groupName);
+            if (bundles != null && !bundles.isEmpty()) {
+                System.out.println(String.format("Bundles for cluster group: " + groupName));
+                System.out.println(String.format(OUTPUT_FORMAT, "Name", "Version", "Status", "Location"));
+                for (String bundle : bundles.keySet()) {
+                    String[] tokens = bundle.split("/");
+                    String name = tokens[0];
+                    String version = tokens[1];
+                    BundleState state = bundles.get(bundle);
+                    String status;
+                    switch (state.getStatus()) {
+                        case BundleEvent.INSTALLED:
+                            status = "Installed";
+                            break;
+                        case BundleEvent.RESOLVED:
+                            status = "Resolved";
+                            break;
+                        case BundleEvent.STARTED:
+                            status = "Started";
+                            break;
+                        case BundleEvent.STARTING:
+                            status = "Starting";
+                            break;
+                        case BundleEvent.STOPPED:
+                            status = "Stopped";
+                            break;
+                        case BundleEvent.STOPPING:
+                            status = "Stopping";
+                            break;
+                        case BundleEvent.UNINSTALLED:
+                            status = "Uninstalled";
+                            break;
+                        default:
+                            status = "";
+                            break;
+                    }
+                    System.out.println(String.format(OUTPUT_FORMAT, name, version, status, state.getLocation()));
+                }
+            } else {
+                System.err.println("No bundles found for cluster group: " + groupName);
+            }
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
+        }
+        return null;
+    }
+
+}
