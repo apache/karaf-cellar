814628dfbbe7fefb732f97d690298d1351308a46
jbonofre <jbonofre@13f79535-47bb-0310-9956-ffa450edef68>
2012-04-27 08:22:47 +0000

    [KARAF-1412] Handler argument is now required in cluster:handler-start and cluster:handler-stop commands
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1331292 13f79535-47bb-0310-9956-ffa450edef68

--- shell/src/main/java/org/apache/karaf/cellar/shell/handler/HandlersStartCommand.java
+++ shell/src/main/java/org/apache/karaf/cellar/shell/handler/HandlersStartCommand.java
@@ -21,7 +21,7 @@ import java.util.List;
 @Command(scope = "cluster", name = "handler-start", description = "Start an event handler.")
 public class HandlersStartCommand extends HandlersSupport {
 
-    @Argument(index = 0, name = "handler", description = "The event handler ID.", required = false, multiValued = false)
+    @Argument(index = 0, name = "handler", description = "The event handler ID.", required = true, multiValued = false)
     String handler;
 
     @Argument(index = 1, name = "node", description = "The node(s) ID.", required = false, multiValued = true)
