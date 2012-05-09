075b078aa0440a20a37e500971c461cb0e10d840
jbonofre <jbonofre@13f79535-47bb-0310-9956-ffa450edef68>
2012-05-09 17:39:49 +0000

    [KARAF-1429] Check is the bundle location is allowed outbound in the CellarBundleMBean
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.2.x@1336310 13f79535-47bb-0310-9956-ffa450edef68

--- bundle/src/main/java/org/apache/karaf/cellar/bundle/shell/UninstallBundleCommand.java
+++ bundle/src/main/java/org/apache/karaf/cellar/bundle/shell/UninstallBundleCommand.java
@@ -77,7 +77,7 @@ public class UninstallBundleCommand extends CellarCommandSupport {
             support.setGroupManager(this.groupManager);
             support.setConfigurationAdmin(this.configurationAdmin);
             if (!support.isAllowed(group, Constants.CATEGORY, location, EventType.OUTBOUND)) {
-                System.err.println("Bundle on " + location + " is blocked outbound");
+                System.err.println("Bundle location " + location + " is blocked outbound");
                 return null;
             }
 
