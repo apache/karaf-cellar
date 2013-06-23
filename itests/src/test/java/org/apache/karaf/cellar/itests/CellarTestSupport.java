22ebe6828219c95075d1c14e497b435fc40b2f35
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-06-23 08:45:36 +0000

    [KARAF-2349] Add debug support in itests
    git-svn-id: https://svn.apache.org/repos/asf/karaf/cellar/branches/cellar-2.3.x@1495800 13f79535-47bb-0310-9956-ffa450edef68

--- itests/src/test/java/org/apache/karaf/cellar/itests/CellarTestSupport.java
+++ itests/src/test/java/org/apache/karaf/cellar/itests/CellarTestSupport.java
@@ -15,6 +15,7 @@ package org.apache.karaf.cellar.itests;
 
 import org.apache.felix.service.command.CommandProcessor;
 import org.apache.felix.service.command.CommandSession;
+import org.apache.karaf.tooling.exam.options.KarafDistributionOption;
 import org.apache.karaf.tooling.exam.options.LogLevelOption;
 import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
@@ -156,11 +157,18 @@ public class CellarTestSupport {
 
     @Configuration
     public Option[] config() {
-        return new Option[]{
+        Option[] options = new Option[]{
                 cellarDistributionConfiguration(), keepRuntimeFolder(), logLevel(LogLevelOption.LogLevel.ERROR),
                 editConfigurationFileExtend("etc/system.properties", "cellar.feature.url", maven().groupId("org.apache.karaf.cellar").artifactId("apache-karaf-cellar").versionAsInProject().classifier("features").type("xml").getURL()),
                 editConfigurationFileExtend("etc/config.properties", "org.apache.aries.blueprint.synchronous", "true")
         };
+        String debug = System.getProperty("debugMain");
+        if (debug != null) {
+            int l = options.length;
+            options = Arrays.copyOf(options, l + 1);
+            options[l] = KarafDistributionOption.debugConfiguration();
+        }
+        return options;
     }
 
     /**
