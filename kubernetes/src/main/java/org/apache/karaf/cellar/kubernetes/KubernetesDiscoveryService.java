7ec918a08faa3c40be9a5236c96aa26c23a6be9e
Jean-Baptiste Onofré <jbonofre@apache.org>
2015-01-18 19:27:34 +0100

    [KARAF-3447] Add kubernetes discovery service

---
+++ kubernetes/src/main/java/org/apache/karaf/cellar/kubernetes/KubernetesDiscoveryService.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.cellar.kubernetes;
+
+import io.fabric8.kubernetes.api.KubernetesClient;
+import io.fabric8.kubernetes.api.KubernetesFactory;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import org.apache.karaf.cellar.core.discovery.DiscoveryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Discovery service that uses the Kubernetes API to discover Cellar nodes.
+ */
+public class KubernetesDiscoveryService implements DiscoveryService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesDiscoveryService.class);
+
+    private String kubernetesHost;
+    private String kubernetesPort;
+    private String kubernetesPodLabelKey;
+    private String kubernetesPodLabelValue;
+
+    private KubernetesClient kubernetesClient;
+
+    public KubernetesDiscoveryService() {
+        LOGGER.debug("CELLAR KUBERNETES: create discovery service");
+    }
+
+    public void init() {
+        try {
+            String kubernetesUrl = "http://" + kubernetesHost + ":" + kubernetesPort;
+            LOGGER.debug("CELLAR KUBERNETES: query API at {} ...", kubernetesUrl);
+            kubernetesClient = new KubernetesClient(new KubernetesFactory(kubernetesUrl));
+            LOGGER.debug("CELLAR KUBERNETES: discovery service initalized");
+        } catch (Exception e) {
+            LOGGER.error("CELLAR KUBERNETES: can't init discovery service", e);
+        }
+    }
+
+    public void destroy() {
+        LOGGER.debug("CELLAR KUBERNETES: destroy discovery service");
+    }
+
+    public void update(Map<String, Object> properties) {
+        LOGGER.debug("CELLAR KUBERNETES: update properties");
+    }
+
+    @Override
+    public Set<String> discoverMembers() {
+        LOGGER.debug("CELLAR KUBERNETES: query pods with labeled with [{}={}]", kubernetesPodLabelKey, kubernetesPodLabelValue);
+        Set<String> members = new HashSet<String>();
+        try {
+            PodList podList = kubernetesClient.getPods();
+            for (Pod pod : podList.getItems()) {
+                String value = pod.getLabels().get(kubernetesPodLabelKey);
+                if (value != null && !value.isEmpty()) {
+                    members.add(pod.getCurrentState().getPodIP());
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("CELLAR KUBERNETES: can't get pods", e);
+        }
+        return members;
+    }
+
+    @Override
+    public void signIn() {
+        // nothing to do for Kubernetes
+    }
+
+    @Override
+    public void refresh() {
+        // nothing to do for Kubernetes
+    }
+
+    @Override
+    public void signOut() {
+        // nothing to do for Kubernetes
+    }
+
+    public String getKubernetesHost() {
+        return kubernetesHost;
+    }
+
+    public void setKubernetesHost(String kubernetesHost) {
+        this.kubernetesHost = kubernetesHost;
+    }
+
+    public String getKubernetesPort() {
+        return kubernetesPort;
+    }
+
+    public void setKubernetesPort(String kubernetesPort) {
+        this.kubernetesPort = kubernetesPort;
+    }
+
+    public String getKubernetesPodLabelKey() {
+        return kubernetesPodLabelKey;
+    }
+
+    public void setKubernetesPodLabelKey(String kubernetesPodLabelKey) {
+        this.kubernetesPodLabelKey = kubernetesPodLabelKey;
+    }
+
+    public String getKubernetesPodLabelValue() {
+        return kubernetesPodLabelValue;
+    }
+
+    public void setKubernetesPodLabelValue(String kubernetesPodLabelValue) {
+        this.kubernetesPodLabelValue = kubernetesPodLabelValue;
+    }
+
+}
---
+++ kubernetes/src/main/java/org/apache/karaf/cellar/kubernetes/KubernetesDiscoveryServiceFactory.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.cellar.kubernetes;
+
+import org.apache.karaf.cellar.core.discovery.DiscoveryService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A factory for Kubernetes discovery services.
+ */
+public class KubernetesDiscoveryServiceFactory implements ManagedServiceFactory {
+
+    private static String getEnvOrDefault(String var, String def) {
+        final String val = System.getenv(var);
+        return val == null ? def : val;
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesDiscoveryServiceFactory.class);
+
+    private static final String KUBERNETES_POD_LABEL_KEY = "pod.label.key";
+    private static final String KUBERNETES_POD_LABEL_VALUE = "pod.label.value";
+
+    private final Map<String, ServiceRegistration> registrations = new ConcurrentHashMap<String, ServiceRegistration>();
+
+    private final BundleContext bundleContext;
+
+    public KubernetesDiscoveryServiceFactory(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    @Override
+    public String getName() {
+        return "CELLAR KUBERNETES: discovery service factory";
+    }
+
+    @Override
+    public void updated(String pid, Dictionary properties) throws ConfigurationException {
+        ServiceRegistration newServiceRegistration = null;
+        try {
+            if (properties != null) {
+                Properties serviceProperties = new Properties();
+                for (Map.Entry entry : serviceProperties.entrySet()) {
+                    Object key = entry.getKey();
+                    Object value = entry.getValue();
+                    serviceProperties.put(key, value);
+                }
+
+                KubernetesDiscoveryService kubernetesDiscoveryService = new KubernetesDiscoveryService();
+
+                String kubernetesHost = getEnvOrDefault("KUBERNETES_RO_SERVICE_HOST", "localhost");
+                String kubernetesPort = getEnvOrDefault("KUBERNETES_RO_SERVICE_PORT", "8080");
+                String kubernetesPodLabelKey = (String) properties.get(KUBERNETES_POD_LABEL_KEY);
+                String kubernetesPodLabelValue = (String) properties.get(KUBERNETES_POD_LABEL_VALUE);
+
+                kubernetesDiscoveryService.setKubernetesHost(kubernetesHost);
+                kubernetesDiscoveryService.setKubernetesPort(kubernetesPort);
+                kubernetesDiscoveryService.setKubernetesPodLabelKey(kubernetesPodLabelKey);
+                kubernetesDiscoveryService.setKubernetesPodLabelValue(kubernetesPodLabelValue);
+                kubernetesDiscoveryService.init();
+
+                newServiceRegistration = bundleContext.registerService(DiscoveryService.class.getName(), kubernetesDiscoveryService, (Dictionary) serviceProperties);
+            }
+        } finally {
+            ServiceRegistration oldServiceRegistration = (newServiceRegistration == null) ? registrations.remove(pid) : registrations.put(pid, newServiceRegistration);
+            if (oldServiceRegistration != null) {
+                LOGGER.debug("CELLAR KUBERNETES: unregister discovery service {}", pid);
+                oldServiceRegistration.unregister();
+            }
+        }
+    }
+
+    @Override
+    public void deleted(String pid) {
+        LOGGER.debug("CELLAR KUBERNETES: delete discovery service {}", pid);
+        ServiceRegistration oldServiceRegistration = registrations.remove(pid);
+        if (oldServiceRegistration != null) {
+            oldServiceRegistration.unregister();
+        }
+    }
+
+}
