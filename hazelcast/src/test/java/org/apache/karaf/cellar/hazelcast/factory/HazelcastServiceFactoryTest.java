4600723716ff9e64a375bfc41339a25f365acd53
Jean-Baptiste Onofré <jbonofre@apache.org>
2015-11-25 10:57:38 +0100

    [KARAF-4144] Fix default hazelcast.xml to work with other XML transformer like Saxon

--- hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastServiceFactoryTest.java
+++ hazelcast/src/test/java/org/apache/karaf/cellar/hazelcast/factory/HazelcastServiceFactoryTest.java
@@ -32,14 +32,37 @@ public class HazelcastServiceFactoryTest {
         // networks where multicast is disabled. Use a custom hazelcast.xml
         // configuration that disables multicast and enables tcp on 127.0.0.1
         System.setProperty("karaf.etc", "src/test/resources/etc");
+        System.setProperty("javax.xml.transform.TransformerFactory", "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl");
         HazelcastServiceFactory factory = new HazelcastServiceFactory();
         factory.setConfigurationManager(new HazelcastConfigurationManager());
         factory.init();
         factory.getInstance();
+        HazelcastInstance factoryInstance = factory.getInstance();
+
         HazelcastInstance defaultInstance = Hazelcast.newHazelcastInstance(null);
 
-        HazelcastInstance factoryInstance = factory.getInstance();
         Assert.assertEquals(true, factoryInstance.getCluster().getMembers().size() >= 2);
+
+        factoryInstance.shutdown();
+        defaultInstance.shutdown();
+    }
+
+    @Test
+    public void testDefaultInstanceWithSaxon() throws InterruptedException {
+        System.setProperty("karaf.etc", "src/test/resources/etc");
+        System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+        HazelcastServiceFactory factory = new HazelcastServiceFactory();
+        factory.setConfigurationManager(new HazelcastConfigurationManager());
+        factory.init();
+        factory.getInstance();
+        HazelcastInstance factoryInstance = factory.getInstance();
+
+        HazelcastInstance defaultInstance = Hazelcast.newHazelcastInstance(null);
+
+        Assert.assertEquals(true, factoryInstance.getCluster().getMembers().size() >= 2);
+
+        factoryInstance.shutdown();
+        defaultInstance.shutdown();
     }
 
 }
